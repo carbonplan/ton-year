013cf3ba3231542a2385b919a40de87e215ffe1b
Freya Chay <freya@carbonplan.org>
2021-10-02 02:00:48 +0000

    changing irf to baseline in test, init files

--- carbonplan_tonyear/tests/test_catalog.py
+++ carbonplan_tonyear/tests/test_catalog.py
@@ -1,10 +1,10 @@
-from carbonplan_tonyear import calculate_tonyears, get_irf_curve
+from carbonplan_tonyear import calculate_tonyears, get_baseline_curve
 
 
 def test_calculate_tonyears():
     time_horizon = 100
     delay = 46
     discount_rate = 0
-    ipcc_2000 = get_irf_curve('ipcc_2000')
+    ipcc_2000 = get_baseline_curve('ipcc_2000')
     m = calculate_tonyears('mc', ipcc_2000, time_horizon, delay, discount_rate)
     assert isinstance(m, dict)
