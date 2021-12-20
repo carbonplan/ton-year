52fcc0dbc38d6ed0e593e33a5862b3703b30362a
badgley <grayson@carbonplan.org>
2021-12-20 12:04:48 -0500

    Add function to compare RF of delay vs avoid

--- tonyear/core.py
+++ tonyear/core.py
@@ -20,25 +20,25 @@ def get_baseline_curve(curve_name: str, t_horizon: int = 1001) -> np.ndarray:
     """
 
     if t_horizon <= 0:
-        raise ValueError('t_horizon must be a postive integer')
+        raise ValueError("t_horizon must be a postive integer")
 
-    if curve_name == 'joos_2013':
+    if curve_name == "joos_2013":
         # parameters from Joos et al., 2013 (Table 5)
         # https://doi.org/10.5194/acp-13-2793-2013
         a = [0.2173, 0.2240, 0.2824, 0.2763]
         tau = [0, 394.4, 36.54, 4.304]
-    elif curve_name == 'ipcc_2007':
+    elif curve_name == "ipcc_2007":
         # parameters from IPCC AR4 2007 (Chapter 2, page 213)
         # https://www.ipcc.ch/site/assets/uploads/2018/02/ar4-wg1-chapter2-1.pdf
         a = [0.217, 0.259, 0.338, 0.186]
         tau = [0, 172.9, 18.51, 1.186]
-    elif curve_name == 'ipcc_2000':
+    elif curve_name == "ipcc_2000":
         # parameters from IPCC LULUCF Special Report 2000 (Chapter 2.3.6.3, Footnote 4)
         # https://archive.ipcc.ch/ipccreports/sres/land_use/index.php?idp=74
         a = [0.175602, 0.137467, 0.18576, 0.242302, 0.258868]
         tau = [0, 421.093, 70.5965, 21.42165, 3.41537]
     else:
-        raise ValueError(f'No baseline curve parameters by the name {curve_name}.')
+        raise ValueError(f"No baseline curve parameters by the name {curve_name}.")
 
     baseline_curve = np.full(t_horizon, a[0])
     for t in range(t_horizon):
@@ -65,24 +65,65 @@ def get_discounted_curve(discount_rate: float, curve: np.ndarray) -> np.ndarray:
 
 
 def print_benefit_report(method_output: dict) -> None:
-    '''print the benefit report'''
-    discount = str(round(method_output['parameters']['discount_rate'] * 100, 1))
-    delay = str(method_output['parameters']['delay'])
-    baseline_atm_cost = str(round(method_output['baseline_atm_cost'], 2))
-    benefit = str(round(method_output['benefit'], 2))
-    num_needed = str(round(method_output['num_for_equivalence'], 1))
+    """print the benefit report"""
+    discount = str(round(method_output["parameters"]["discount_rate"] * 100, 1))
+    delay = str(method_output["parameters"]["delay"])
+    baseline_atm_cost = str(round(method_output["baseline_atm_cost"], 2))
+    benefit = str(round(method_output["benefit"], 2))
+    num_needed = str(round(method_output["num_for_equivalence"], 1))
 
     print()
-    print('Discount rate: ' + discount + '%')
-    print('Delay: ' + delay + ' year(s)')
-    print('Baseline atmospheric cost: ' + baseline_atm_cost + ' ton-years')
-    print('Benefit from 1tCO2 with delay: ' + benefit + ' ton-years')
-    print('Number needed: ' + num_needed)
+    print("Discount rate: " + discount + "%")
+    print("Delay: " + delay + " year(s)")
+    print("Baseline atmospheric cost: " + baseline_atm_cost + " ton-years")
+    print("Benefit from 1tCO2 with delay: " + benefit + " ton-years")
+    print("Number needed: " + num_needed)
     print()
+
+
+def get_avoided_comparison(
+    tonyear_calc: dict,
+    baseline_curve: np.array,
+    integration_time: int,
+) -> dict:
+    """
+    Compare cumulative radiative forcing of delaying 1 tCO2  against cumulative radiative forcing of permanently
+     avoiding the emission of a smaller amount of CO2.
+    Parameters
+    ----------
+    tonyear_calc: dict
+        output from `calculate_tonyear`
+    baseline_curve: np.array
+        CO2 IRF curve
+    integration_time: int
+        time period to evaluate equivalence claim; does _not_ have to equal time_horizon from IRF curve generation
+
+    Returns
+    -------
+        dict with cumulative radiative forcing for delay and avoid
+    """
+    DELAY_SIZE = 1  # tC02
+    delay_length = tonyear_calc["parameters"]["delay"]
+    avoided_size = DELAY_SIZE / tonyear_calc["num_for_equivalence"]
+
+    baseline_crf = np.trapz(baseline_curve[:integration_time])
+    avoid_crf = np.trapz(baseline_curve[:integration_time] * avoided_size)
+
+    integate_to = integration_time - delay_length
+    delay_crf = baseline_crf - np.trapz(baseline_curve[:integate_to])
+    return {
+        "delay_length": delay_length,
+        "avoided_crf": avoid_crf,
+        "delay_crf": delay_crf,
+    }
 
 
 def calculate_tonyears(
-    method: str, baseline: np.ndarray, time_horizon: int, delay: int, discount_rate: float
+    method: str,
+    baseline: np.ndarray,
+    time_horizon: int,
+    delay: int,
+    discount_rate: float,
 ):
     """This function calculates the benefit of a delayed emission according one
     of three ton-year accounting methods.
@@ -116,11 +157,13 @@ def calculate_tonyears(
     """
 
     if delay < 0:
-        raise ValueError('Delay cannot be negative.')
+        raise ValueError("Delay cannot be negative.")
     if time_horizon <= 0:
-        raise ValueError('Time horizon must be greater than zero.')
+        raise ValueError("Time horizon must be greater than zero.")
     if len(baseline) < time_horizon:
-        raise ValueError('Time horizon cannot be longer than length of the baseline array.')
+        raise ValueError(
+            "Time horizon cannot be longer than length of the baseline array."
+        )
 
     # All methods calculate the baseline cost of emitting 1tCO2 at t=0 as the
     # atmospheric ton-years incurred over the period 0<=t<=time_horizon.
@@ -129,7 +172,7 @@ def calculate_tonyears(
     baseline_discounted = get_discounted_curve(discount_rate, baseline)
     baseline_atm_cost = np.trapz(baseline_discounted)
 
-    if method == 'mc':
+    if method == "mc":
         # The Moura-Costa method calculates the ton-year benefit of a delayed emission
         # as the ton-years of carbon storage outside of the atmosphere over the period
         # 0<=t<=delay. Moura-Costa ignores the atmospheric impact of post-storage re-emission.
@@ -140,7 +183,7 @@ def calculate_tonyears(
         scenario = get_discounted_curve(discount_rate, scenario)
         benefit = -np.trapz(scenario[:delay_timesteps])
 
-    elif method == 'ipcc':
+    elif method == "ipcc":
         # The IPCC method calculates calculates the ton-year benefit of an emission at t=delay
         # as the difference between the baseline atmospheric cost and the scenario atmospheric
         # cost, which is calculated over the period delay<=t<=time_horizon.
@@ -148,7 +191,7 @@ def calculate_tonyears(
         scenario = get_discounted_curve(discount_rate, scenario)
         benefit = baseline_atm_cost - np.trapz(scenario[delay:])
 
-    elif method == 'lashof':
+    elif method == "lashof":
         # The Lashof method calculates the ton-year benefit of an emission at t=delay
         # as the atmospheric cost that no longer occurs within the time horizon. The
         # benefit calculation is thus focused the period time_horizon<=t<=(time_horizon+delay).
@@ -157,24 +200,24 @@ def calculate_tonyears(
         benefit = np.trapz(scenario[time_horizon:])
 
     else:
-        raise ValueError(f'No ton-year accounting method called {method}')
+        raise ValueError(f"No ton-year accounting method called {method}")
 
     return {
-        'parameters': {
-            'method': method,
-            'time_horizon': time_horizon,
-            'delay': delay,
-            'discount_rate': discount_rate,
+        "parameters": {
+            "method": method,
+            "time_horizon": time_horizon,
+            "delay": delay,
+            "discount_rate": discount_rate,
         },
-        'baseline': baseline_discounted,
-        'scenario': scenario,
-        'baseline_atm_cost': baseline_atm_cost,
-        'benefit': benefit,
-        'num_for_equivalence': baseline_atm_cost / benefit,
+        "baseline": baseline_discounted,
+        "scenario": scenario,
+        "baseline_atm_cost": baseline_atm_cost,
+        "benefit": benefit,
+        "num_for_equivalence": baseline_atm_cost / benefit,
     }
 
 
 def write_json(collection, output):
-    '''helper function to write collection to a local json file'''
+    """helper function to write collection to a local json file"""
     with open(output, "w") as f:
         f.write(json.dumps(collection))
